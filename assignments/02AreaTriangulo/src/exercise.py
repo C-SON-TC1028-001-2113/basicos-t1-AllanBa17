3186513e81e23833c95204b173d27b84847080bd
AllanBa17 <a01254006@itesm.mx>
2021-08-19 21:58:15 +0000

    tarea 1 AreaTriangulo y Promedio

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
+    b = int(input('Dame la base: '))
+    h = int(input('Dame la altura: '))
 
-
+    area = b*h/2
+    print ("El área es: "+str(area))
 if __name__ == '__main__':
     main()
